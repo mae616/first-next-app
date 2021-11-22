c2c48851029aecf420d450ed17083cfef9cc46fe
mae616 <oya2.mi@hotmail.co.jp>
2021-11-22 22:48:28 +0900

    deploy test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-  target: 'serverless',
   webpack: (config, options) => {
     if (!options.dev && options.isServer) {
       const originalEntry = config.entry
       config.entry = async () => {
         const entries = { ...(await originalEntry()) }
-        entries['./scripts/build-rss'] = './scripts/build-rss.js'
+        entries['scripts/build-rss'] = './scripts/build-rss.js'
         return entries
       }
     }
