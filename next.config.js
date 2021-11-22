36f9882d409370fa0158f3e6fc6b65427ddb3223
mae616 <oya2.mi@hotmail.co.jp>
2021-11-22 21:28:47 +0900

    next.config.js変更

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-  target: 'serverless'
+  target: 'serverless',
+  webpack: (config) => {
+    config.entry = async () => {
+      const entries = { ...(await originalEntry()) }
+      entries['./scripts/build-rss'] = './scripts/build-rss.js'
+      return entries
+    }
+  }
 }
