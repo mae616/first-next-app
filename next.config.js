b346f12cb9ded84b9ba11b6d55c45ea2ea87463f
mae616 <oya2.mi@hotmail.co.jp>
2021-11-23 18:52:08 +0900

    戻し

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  target: 'serverless',
-  webpack: (config, options) => {
-    if (!options.dev && options.isServer) {
-      const originalEntry = config.entry
-      config.entry = async () => {
-        const entries = { ...(await originalEntry()) }
-        entries['scripts/build-rss'] = './scripts/build-rss.js'
-        return entries
-      }
-    }
-    return config
-  }
+  reactStrictMode: true
 }
